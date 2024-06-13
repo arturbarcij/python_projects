3938e9a0b719e0258bfa3c05bdaf4b52cc515b8d
Artur Barcij <arturbarcij1@gmail.com>
2024-06-13 18:46:59 +0200

    update

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,3 +1,4 @@
+#calculator App
 class Calculator:
     '''
     Calculator class
@@ -21,15 +22,37 @@ class Calculator:
         if b == 0:
             return "Error: Divison by zero"
         return a / b
+    
+    def modulo(self, a, b):
+        if b == 0:
+            raise ValueError("The divisor 'b' cannot be zero.")
+    
+        quotient = a // b  # This performs integer division
+        remainder = a - quotient * b
+        return remainder
+
+    
+    def sqrt_newton(num, tolerance=1e-10):
+        if num < 0:
+            raise ValueError("Cannot compute the square root of a negative number")
+    
+        guess = num
+        while True:
+            new_guess = (guess + num / guess) / 2
+            if abs(new_guess - guess) < tolerance:
+                return new_guess
+            guess = new_guess
 
 calc = Calculator()
 
 x, y = map(int, input("Enter two integers").split())
 print("First integer: ", x)
 print("Second integer: ", y)
-print(calc.add(x, y))
-print(calc.add(10, 5))
-print(calc.subtract(10, 5))
-print(calc.multiply(10, 5))
-print(calc.division(10, 5))
+print("Add: ", calc.add(x, y))
+print("Subtract: ", calc.subtract(x, y))
+print("Multiply: ", calc.multiply(x, y))
+print("Division: ", calc.division(x, y))
+print("Modulo: ", calc.modulo(x, y))
+print("Sqrt of first int: ", calc.sqrt_newton(x))
+print("Sqrt of second int: ", calc.sqrt_newton(y)) 
 
