f28d098ca76e08f5d47a732817a9036e0d8d8c9f
Artur Barcij <arturbarcij1@gmail.com>
2024-06-17 07:51:03 +0200

    collision 0.1

--- Snake_game/logic/collision.py
+++ Snake_game/logic/collision.py
@@ -1,6 +1,13 @@
 '''
 Collision class is logic of collision in snake game.
 '''
-class Collision:
-    def __init__(self) -> None:
-        pass
+def check_collision_with_walls(x, y, display_width, display_height):
+    if x >= display_width or x < 0 or y >= display_height or y < 0:
+        return True
+    return False
+
+def check_collision_with_self(snake_list):
+    snake_head = snake_list[-1]
+    if snake_head in snake_list[:-1]:
+        return True
+    return False
--- Snake_game/logic/game_logic.py
+++ Snake_game/logic/game_logic.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from game.interface import Interface
+from logic.collision import check_collision_with_walls, check_collision_with_self
 
 class GameLogic:
     def __init__(self):
@@ -49,7 +50,7 @@ class GameLogic:
 
         while not game_over:
 
-            while game_close == True:
+            while game_close:
                 self.interface.display.fill(self.blue)
                 self.message("You Lost! Press Q-Quit or C-Play Again", self.red)
                 self.your_score(Length_of_snake - 1)
@@ -80,22 +81,20 @@ class GameLogic:
                         y1_change = self.snake_block
                         x1_change = 0
 
-            if x1 >= self.display_width or x1 < 0 or y1 >= self.display_height or y1 < 0:
+            if check_collision_with_walls(x1, y1, self.display_width, self.display_height):
                 game_close = True
+
             x1 += x1_change
             y1 += y1_change
             self.interface.display.fill(self.white)
             pygame.draw.rect(self.interface.display, self.green, [foodx, foody, self.snake_block, self.snake_block])
-            snake_Head = []
-            snake_Head.append(x1)
-            snake_Head.append(y1)
+            snake_Head = [x1, y1]
             snake_List.append(snake_Head)
             if len(snake_List) > Length_of_snake:
                 del snake_List[0]
 
-            for x in snake_List[:-1]:
-                if x == snake_Head:
-                    game_close = True
+            if check_collision_with_self(snake_List):
+                game_close = True
 
             self.our_snake(self.snake_block, snake_List)
             self.your_score(Length_of_snake - 1)
