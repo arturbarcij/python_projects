21aafcc23ef22a748b6bba0c6638e9307b75c614
Artur Barcij <arturbarcij1@gmail.com>
2024-06-22 21:05:05 +0200

    game logic class

--- Tic_Tac_Toe/interface/display.py
+++ Tic_Tac_Toe/interface/display.py
@@ -1,2 +1,9 @@
+import pygame
+
 class Display:
+    def __init__(self, width, height):
+        self.display = pygame.display.set_mode(width, height)
+        pygame.display.set_caption("Tic Tac Toe")
     
+    def update_display(self):
+        pygame.display.update()
---
+++ Tic_Tac_Toe/logic/game_logic.py
@@ -0,0 +1,11 @@
+import pygame
+
+class GameLogic:
+    def __init__:
+    
+
+    def left(self):
+    
+    def right(self):
+    
+    def diagonal(self):
