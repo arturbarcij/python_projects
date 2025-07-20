a73f72fb29a8a6158f5c2e28e8b6416594bfa0ef
Artur Barcij <arturbarcij1@gmail.com>
2025-07-21 00:24:57 +0200

    Initial commit for all projects

--- Tic_Tac_Toe/logic/game_logic.py
+++ Tic_Tac_Toe/logic/game_logic.py
@@ -2,10 +2,15 @@ import pygame
 
 class GameLogic:
     def __init__:
+        black = (0, 0, 0)
+        white =  (255, 255, 255)
+        width = 1280
+        height = 720
     
+        board = pygame.self[width, height]
 
     def left(self):
-    
+        for i in range(board)
     def right(self):
     
     def diagonal(self):
---
+++ task_manager.py
@@ -0,0 +1,11 @@
+arrOfTasks = []
+typeOfTasks = ["Urgent", "Important", "Necessary", ""]
+
+def task(request):
+    if request == "Important":
+        arrOfTasks.append
+
+def get_task():
+def set_task():
+def modify_task():
+def delete_task():
